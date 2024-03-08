c1d55cebbef52e007e6f16544a8fd972731d95ce
nawabsahab16 <Nawab162004@gmail.com>
2024-03-08 21:51:58 +0530

    added img for stunning website visuals!😍

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
-
-
-function App() {
-  
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello ji!
-    </h1>
-    )
-}
+    <>
+      <h1 className="text-3xl font-bold underline"> Hello ji! </h1>
+      <ButtonGradient />
+    </>
+  );
+};
 
-export default App
+export default App;
